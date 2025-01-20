aa1680738f602a0a6b60ee427c0404796eadc868
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-01-20 18:19:43 +0300

    Дороработка cardMain, добавление стилей для последних двух карточек

--- src/components/Main/CardDescription/CardDescription.jsx
+++ src/components/Main/CardDescription/CardDescription.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-export const CardDescription = ({ title, description }) => {
+export const CardDescription = ({ title, description, isAbsolute, cardId }) => {
+    const styleAbsolute = isAbsolute
+        ? "absolute  bottom-0 text-center justify-center items-center"
+        : "";
+    const styleForLastCard = isAbsolute ? "w-1/2" : "";
+    const styleForCard5 = cardId === 5 && "text-darkDesaturatedCyan";
+    const styleForCard6 = cardId === 6 && "text-darkBlue";
+
     return (
-        <div className="flex flex-col gap-6 w-[90%]">
-            <h2 className="text-clamp-sm">{title}</h2>
-            <p className="description">{description}</p>
+        <div className={`${styleAbsolute} flex flex-col gap-6 `}>
+            <h2
+                className={`${
+                    cardId === 5 && "text-darkDesaturatedCyan font-bold"
+                } ${cardId === 6 && "text-darkBlue font-bold"}  text-clamp-sm`}
+            >
+                {title}
+            </h2>
+            <p
+                className={`${styleForLastCard} ${styleForCard5} ${styleForCard6}`}
+            >
+                {description}
+            </p>
         </div>
     );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,30 +4,49 @@ import { motion } from "framer-motion";
 import { ReviewSection } from "./ReviewSection/ReviewSection";
 import { CardDescription } from "./CardDescription/CardDescription";
 
-import { cardData } from "../../data/cardMain";
-
 export const Main = () => {
     const commonClasses =
-        "text-clamp-xs sm:flex sm:justify-center sm:items-center px-20 py-20 bg-cover bg-no-repeat bg-center";
+        "text-clamp-xs px-20 py-20 bg-cover bg-no-repeat bg-center";
 
     return (
         <motion.main>
             <section className="grid sm:grid-cols-1  lg:grid-cols-2 lg:grid-rows-3 xl:grid-cols-2">
-                {cardData.map(({ title, description, bgImage }, index) => (
-                    <motion.div
-                        key={index}
-                        className={`${commonClasses} ${bgImage || ""} ${
-                            index >= cardData.length - 1 ? "" : ""
-                        }`}
-                    >
-                        {title && description && (
-                            <CardDescription
-                                title={title}
-                                description={description}
-                            />
-                        )}
-                    </motion.div>
-                ))}
+                <div id="1" class={`${commonClasses}`}>
+                    <CardDescription
+                        title="Transform your brand"
+                        description="We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
+                    />
+                </div>
+                <div id="2" class={`${commonClasses} bg-mainCardImage1`}></div>
+                <div id="3" class={`${commonClasses} bg-mainCardImage2`}></div>
+                <div id="4" class={`${commonClasses}`}>
+                    <CardDescription
+                        title="Stand out to the right audience"
+                        description="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places."
+                    />
+                </div>
+                <div
+                    id="5"
+                    class={`${commonClasses} bg-mainCardImage3 relative overflow-hidden`}
+                >
+                    <CardDescription
+                        cardId={5}
+                        isAbsolute={true}
+                        title="Graphic design"
+                        description="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention"
+                    />
+                </div>
+                <div
+                    id="6"
+                    class={`${commonClasses} bg-mainCardImage4 relative overflow-hidden`}
+                >
+                    <CardDescription
+                        cardId={6}
+                        isAbsolute={true}
+                        title="Photography"
+                        description="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+                    />
+                </div>
             </section>
 
             <ReviewSection />
